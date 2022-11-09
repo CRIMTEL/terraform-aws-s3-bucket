85927759236630b9e57938a14fbc7c7b9eae7333
CRIMTEL <crim@telenor.dk>
2022-11-09 13:44:11 +0100

    bucket policies added to s3_bucket resource

--- main.tf
+++ main.tf
@@ -12,9 +12,6 @@ locals {
   intelligent_tiering = try(jsondecode(var.intelligent_tiering), var.intelligent_tiering)
 }
 
-#bucket policies:
-attach_policy = true
-policy = data.aws_iam_policy_document.bucket_policy
 
 data "aws_iam_policy_document" "bucket_policy" {
   statement {
@@ -38,6 +35,10 @@ resource "aws_s3_bucket" "this" {
   force_destroy       = var.force_destroy
   object_lock_enabled = var.object_lock_enabled
   tags                = var.tags
+
+  #bucket policies:
+  attach_policy = true
+  policy = data.aws_iam_policy_document.bucket_policy
 }
 
 #Add index.html to the bucket upon creation
