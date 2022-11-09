4f2fcc1b18a978a53111efea96bdfea787c548d9
CRIMTEL <crim@telenor.dk>
2022-11-09 13:40:26 +0100

    "backup_main.tf" added to .gitignore

--- main.tf
+++ main.tf
@@ -5,14 +5,6 @@ locals {
 
   attach_policy = var.attach_require_latest_tls_policy || var.attach_elb_log_delivery_policy || var.attach_lb_log_delivery_policy || var.attach_deny_insecure_transport_policy || var.attach_policy
 
-  #grant = {
-  #  grantee {
-  #    id    =
-  #    type  =
-  #  }
-  #  permission = "READ"
-  #}
-
   # Variables with type `any` should be jsonencode()'d when value is coming from Terragrunt
   grants              = try(jsondecode(var.grant), var.grant)
   cors_rules          = try(jsondecode(var.cors_rule), var.cors_rule)
